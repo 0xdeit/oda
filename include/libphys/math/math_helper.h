4887cbcb7a4f6c779252622b37f6990610df60e4
Diego Garza <deit@protonmail.com>
2021-03-15 13:41:48 -0600

    [libphys] refactor angle switch cases and spherical conversion methods
    Most switch cases were just 1 specific case and 1 default, changed to a simple if statement for the specific case. Implemented from/to cartesian conversion methods for Spherical coordinates

--- include/libphys/math/math_helper.h
+++ include/libphys/math/math_helper.h
@@ -6,6 +6,7 @@
 #define ODA_MATH_HELPER_H
 
 #include "libphys/math/Scalar2.h"
+#include "libphys/math/Scalar3.h"
 #include <cmath>
 
 namespace usc::types {
@@ -28,66 +29,94 @@ namespace usc::conversion {
     }
 
     // Convert cartesian coordinates with the origin at (0, 0) and  end in (x, y) form to polar coordinates (r, theta).
-    static types::Scalar2 CartesianToPolar(double x, double y, types::AngleType angle_type = types::kDegrees) {
+    static types::Scalar2 CartesianToPolar(double x, double y, types::AngleType angle_output_type = types::kDegrees) {
         types::Scalar2 result{0, 0};
 
         result.first = sqrt((pow(x, 2) + pow(y, 2)));
 
-        switch (angle_type) {
-            case types::kDegrees:
-                // atan2 returns the arc in radians.
-                result.last = RadiansToDegrees(atan2(y, x));
-                break;
-            case types::kRadians:
-                result.last = atan2(y, x);
-                break;
+        // atan2 returns in radians.
+        result.last = atan2(y, x);
+
+        // give user the angle type they want
+        if (angle_output_type == types::kDegrees){
+            result.last = RadiansToDegrees(result.last);
         }
 
         return result;
     }
 
     // Convert cartesian coordinates with given origin and end in (x, y) form to polar coordinates (r, theta).
-    static types::Scalar2 CartesianToPolar(types::Scalar2 &origin, types::Scalar2 &end, types::AngleType angle_type = types::kDegrees) {
+    static types::Scalar2
+    CartesianToPolar(types::Scalar2 &origin, types::Scalar2 &end, types::AngleType angle_output_type = types::kDegrees) {
         types::Scalar2 result{0, 0};
 
-        double x = end.first - origin.first;
-        double y = end.last - origin.last;
+        double delta_x = end.first - origin.first;
+        double delta_y = end.last - origin.last;
 
-        result.first = sqrt((pow(x, 2) + pow(y, 2)));
+        result.first = sqrt((pow(delta_x, 2) + pow(delta_y, 2)));
 
-        switch (angle_type) {
-            case types::kDegrees:
-                // atan2 returns the arc in radians.
-                result.last = RadiansToDegrees(atan2(y, x));
-                break;
-            case types::kRadians:
-                result.last = atan2(y, x);
-                break;
+        // atan2 returns the arc in radians.
+        result.last = atan2(delta_y, delta_x);
+
+        // give user the angle type they want
+        if (angle_output_type == types::kDegrees){
+            result.last = RadiansToDegrees(result.last);
         }
 
         return result;
     }
 
-    // TODO: Create analog PolarToCartesian function with a given origin if really needed?
-
     // Convert polar coordinates (r, theta) to cartesian coordinates (x, y)
     static types::Scalar2
-    PolarToCartesian(double length, double angle, types::AngleType angle_type = types::kDegrees) {
+    PolarToCartesian(double length, double angle, types::AngleType angle_input_type = types::kDegrees) {
         types::Scalar2 result{0, 0};
-        // TODO: maybe remove this variable and just use angle param?
-        double final_angle = 0;
 
-        switch (angle_type) {
-            case types::kDegrees:
-                final_angle = DegreesToRadians(angle);
-                break;
-            case types::kRadians:
-                final_angle = angle;
-                break;
+        // Both cos() and sin() take the angle param in radians.
+        if (angle_input_type == types::kDegrees) {
+            angle = DegreesToRadians(angle);
         }
 
-        result.first = length * cos(final_angle);
-        result.last = length * sin(final_angle);
+        // component x
+        result.first = length * cos(angle);
+        // component y
+        result.last = length * sin(angle);
+
+        return result;
+    }
+
+    // Convert spherical coordinates (radial distance, polar angle, azimuth angle) to cartesian (x, y, z)
+    // Names for the spherical components come from the ISO 80000-2:2019 convention (src: Wikipedia)
+    static types::Scalar3
+    SphericalToCartesian(double radius, double theta, double phi, types::AngleType angle_input_type = types::kDegrees) {
+        types::Scalar3 result{0, 0, 0};
+
+        // Both cos() and sin() take the angle param in radians.
+        if (angle_input_type == types::kDegrees) {
+            theta = DegreesToRadians(theta);
+            phi = DegreesToRadians(phi);
+        }
+
+        // component x
+        result.first = (radius * sin(theta) * cos(phi));
+        // component y
+        result.middle = (radius * sin(theta) * sin(phi));
+        // component z
+        result.last = (radius * cos(theta));
+
+        return result;
+    }
+
+    // Convert cartesian (x, y, z) to spherical coordinates (radial distance, polar angle, azimuth angle)
+    static types::Scalar3
+    CartesianToSpherical(double x, double y, double z) {
+        types::Scalar3 result{0, 0, 0};
+
+        // radius
+        result.first = sqrt((pow(x, 2) + pow(y, 2) + pow(z, 2)));
+        // polar angle / theta
+        result.middle = atan2(sqrt(pow(x, 2) + pow(y, 2)), z);
+        // azimuth angle / phi
+        result.last = atan2(y, x);
 
         return result;
     }
