25808ec3647c5a782b07e84eb36b40e24f6345d4
Diego Garza <deit@protonmail.com>
2021-03-19 08:02:32 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 //
 // Created by dgt on 3/12/2021.
 //
-#include "libphys/test/Vector2_test.h"
+
+#include "LibPhys/Math/Vector3.h"
+#include "LibPhys/Math/Conversions.h"
+
+// dev purposes only
+namespace ut = usc::types;
+namespace uc = usc::conversion;
 
 int main() {
-    usc::test::Vector2Methods();
+    auto origin = ut::Cartesian3{0, 0, 0};
+    auto pt_a = ut::Cartesian3{1, 2, 1};
+    auto pt_b = ut::Cartesian3{5, 4, 2};
+
+    auto vec3_a = ut::Vector3(origin, pt_a);
+    auto vec3_b = ut::Vector3(origin, pt_b);
+    auto vec3_ab = ut::Vector3(pt_a, pt_b);
+
+    vec3_a.Info();
+    vec3_b.Info();
+    vec3_ab.Info();
 
     return 0;
 }
